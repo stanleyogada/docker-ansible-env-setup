347457e9b65b7a5ead3f4d9ad5af03561db471d6
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-01-13 01:45:47 +0100

    Merge install in 1 script for server

--- scripts/bootstrap-server.sh
+++ scripts/bootstrap-server.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 
+# Install required server packages
+
+
+dnf install -y sudo vim rsync;
+if [[ ! $? -eq 0 ]]; then 
+	apt update && apt install -y iproute2 openssh-server sudo vim rsync;
+fi; 
+
+echo
+echo "You can now use 'ip', and 'sshd' packages!"
+
+
+
+
+
 # Setup sshd service
 
 # Creates the /run/sshd dir as it's required to start the sshd service using `/sbin/sshd -D`
@@ -26,6 +41,11 @@ fi;
 echo
 echo "Enter user 'zero' passwd: "
 useradd -ms /bin/bash -G sudo zero && passwd zero;
+if [[ ! $? -eq 0 ]]; then useradd -ms /bin/bash -G wheel zero && passwd zero; fi; 
 echo
 echo "Enter user 'zero' passwd: "
 su - zero;
+#!/bin/bash
+
+
+
