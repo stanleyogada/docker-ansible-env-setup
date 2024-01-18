240ff279c6a73ea1b36cffcc202b69e5e9483a3f
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-01-18 19:27:45 +0100

    Remove bashrc-content file
    - Write in the bashrc content right in the inject script

--- scripts/bootstrap-script-inject.sh
+++ scripts/bootstrap-script-inject.sh
@@ -4,19 +4,41 @@
 #script_home_path="$HOME/scripts"
 
 bashrc_path="$HOME/.bashrc";
-bashrc_content_path="$HOME/.bootstrap-bashrc-content.txt";
 script_path="$1";
 
 if [[ -z $script_path ]]; then 
 	echo "No script to inject to $bashrc_path! Pass the name of the script as arg.";
 	cd $HOME;
 	/bin/bash;
-       	exit 128; 
+       	exit 1; 
 fi;
 
-has_content=`grep "$script_path" $bashrc_path`;
+has_content=$(grep "$script_path" "$bashrc_path");
 if [[ -z $has_content ]]; then
-	cat $bashrc_content_path | sed "s/_SCRIPT_PATH/$script_path/g" >> $bashrc_path;
+	cat >> "$bashrc_path" <<EOF
+cd $HOME;
+
+echo "This is a request to run auto-run script ($script_path) which was injected to this machine's ($HOME/.bashrc)."
+echo "You don't have to run if you ran before, type (no)."
+echo
+echo "Type (yes) for the first time or if your wish to re-run the script."
+echo
+read -p "Do you want to continue? (yes/no) " ans;
+
+if [[ ! \$ans == "yes" ]]; then
+	echo "(no) was assumed!";
+else
+	$script_path;
+
+	# If SSHD is not running and SSHD is installed
+	if [[ -z \$(pgrep sshd) && -e /sbin/sshd ]]; then
+		echo "Starting SSH daemon."
+		/sbin/sshd -D &
+	else
+		echo "SSHD already running OR SSHD is not installed yet!"
+	fi;
+fi;
+EOF
 
 	echo "$script_path injected to $bashrc_path!";
 else
